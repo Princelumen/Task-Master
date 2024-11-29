4917180fcac1c68d502664acba11b113d8d87c96
prince <princeoguignatius@gmail.com>
2024-11-29 15:18:05 +0000

    server deployed

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "https://taskfyer.onrender.com";
+  const serverUrl = "https://task-master-pc62.onrender.com";
 
   const router = useRouter();
 
